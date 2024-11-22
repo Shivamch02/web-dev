5af7384423d3aad0a2ca8b31b35ef9a706dd7b81
Shivam Chaurasiya <chshivam53@gmail.com>
2024-11-22 10:57:35 +0530

    created button component

--- nextjs/components/Signin.tsx
+++ nextjs/components/Signin.tsx
@@ -1,7 +1,8 @@
+"use client";
+
+import Button from "./Button";
+
 export const SigninComponent = () => {
-  function handler() {
-    console.log("onClick Handler");
-  }
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -24,13 +25,7 @@ export const SigninComponent = () => {
                 type={"password"}
                 placeholder="123456"
               />
-              <button
-                onClick={handler}
-                type="button"
-                className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-              >
-                Sign in
-              </button>
+              <Button />
             </div>
           </div>
         </a>
