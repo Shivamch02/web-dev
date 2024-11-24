c5e585263ea962f4746fe4f5af77f7de0a043bed
Shivam Chaurasiya <chshivam53@gmail.com>
2024-11-24 11:39:04 +0530

    fixes data fetching

--- nextjs-backend/app/page.tsx
+++ nextjs-backend/app/page.tsx
@@ -1,19 +1,19 @@
-import axios from "axios";
+// import axios from "axios";
 
-async function getUserDetails() {
-  const response = await axios.get("http://localhost:3000/api/user");
-  return response.data;
-}
+// export async function getUserDetails() {
+//   const response = await axios.get("http://localhost:3000/api/user");
+//   return response.data;
+// }
 
 export default async function Home() {
-  const userData = await getUserDetails();
+  // const userData = await getUserDetails();
   return (
     <div className="flex flex-col justify-center h-screen">
       <div className="flex justify-center">
         <div className="border p-8 rounded">
-          <div>Name: {userData?.name}</div>
+          {/* <div>Name: {userData?.name}</div> */}
 
-          {userData?.email}
+          {/* {userData?.email} */}
         </div>
       </div>
     </div>
